da89b9728f42ec1d753d5e4998ceaf6d8fb3bde3
slmens <selimenes527@outlook.com>
2024-01-26 14:02:35 +0300

    Did create a job that fires every minute and can find call reminders

--- CallFriendApp/src/main/java/com/slmens/CallFriendApp/CallFriendAppApplication.java
+++ CallFriendApp/src/main/java/com/slmens/CallFriendApp/CallFriendAppApplication.java
@@ -1,41 +1,25 @@
 package com.slmens.CallFriendApp;
 
-import com.slmens.CallFriendApp.config.TimerInfo;
-import com.slmens.CallFriendApp.dao.CallReminderRepository;
 import com.slmens.CallFriendApp.dao.UserRepository;
-import com.slmens.CallFriendApp.entities.CallReminder;
 import com.slmens.CallFriendApp.entities.Role;
 import com.slmens.CallFriendApp.entities.User;
-import com.slmens.CallFriendApp.jobs.CallReminderJob;
-import com.slmens.CallFriendApp.jobs.HelloWorldJob;
-import com.slmens.CallFriendApp.service.concretes.SchedulerService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.function.Function;
-
 @SpringBootApplication
+@EnableScheduling
 public class CallFriendAppApplication implements CommandLineRunner {
 	@Value("${reminduck.app.admin.pass}")
 	String AdminPass;
 
 	private final UserRepository userRepository;
-	private final SchedulerService schedulerService;
 
-	public CallFriendAppApplication(UserRepository userRepository, SchedulerService schedulerService) {
+	public CallFriendAppApplication(UserRepository userRepository) {
 		this.userRepository = userRepository;
-		this.schedulerService = schedulerService;
 	}
 
 	public static void main(String[] args) {
@@ -60,15 +44,5 @@ public class CallFriendAppApplication implements CommandLineRunner {
 
 			this.userRepository.save(user);
 		}
-
-		final TimerInfo info = new TimerInfo();
-		info.setRunForever(true);
-		info.setRemainingFireCount(info.getTotalFireCount());
-		info.setRepeatIntervalMs(30000);
-		info.setInitialOffsetMs(100);
-		info.setCallbackData("My callback data");
-
-
-		schedulerService.schedule(CallReminderJob.class, info);
 	}
 }
--- CallFriendApp/src/main/java/com/slmens/CallFriendApp/jobs/CallReminderJob.java
+++ CallFriendApp/src/main/java/com/slmens/CallFriendApp/jobs/CallReminderJob.java
@@ -8,43 +8,36 @@ import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-
 @Component
-public class CallReminderJob implements Job {
+public class CallReminderJob{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CallReminderJob.class);
 
+    @Autowired
     private CallReminderRepository callReminderRepository; // Assuming you have a repository for CallReminder
 
-    // Default constructor
-    public CallReminderJob() {
-        this.callReminderRepository = null; // or initialize it if needed
-    }
-
-    // Constructor with parameter
-    public CallReminderJob(CallReminderRepository callReminderRepository) {
-        this.callReminderRepository = callReminderRepository;
-    }
-
-    @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    @Scheduled(fixedDelay = 60000)
+    public void execute() throws JobExecutionException {
         LOGGER.info("Executing CallReminderJob...");
-
-        CallReminderRepository callReminderRepository =
-                (CallReminderRepository) jobExecutionContext.getJobDetail().getJobDataMap().get("callReminderRepository");
 
         List<CallReminder> remindersToCall = this.callReminderRepository.findAll();
         LocalTime currentTime = LocalTime.now();
         DayOfWeek currentDayOfWeek = java.time.LocalDate.now().getDayOfWeek();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime formattedTime = LocalTime.parse(currentTime.format(formatter));
 
         for (CallReminder reminder : remindersToCall) {
-            if (reminder.getCallReminderTime().equals(currentTime) &&
+            if (reminder.getCallReminderTime().equals(formattedTime) &&
                     reminder.getCallReminderDays().contains(currentDayOfWeek.toString())) {
                 // Perform the action for the matching reminder
                 LOGGER.info("Calling {} - {}", reminder.getWhoToCall(), reminder.getDescription());
